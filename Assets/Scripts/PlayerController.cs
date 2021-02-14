731ca87bf47f4cda80565af693f812f40ba66090
Carlisle Hughes <22939626+carlislehughes@users.noreply.github.com>
2021-02-14 15:30:35 -0800

    Lesson D finished
    PlayerController Script updates, blockades moved under environment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 15.0f;
+    //Speed variables -- rate of forward/backward movement
+    private float speed = 15.0f;
+    private float turnSpeed = 15.0f;
+
+    //Input variables
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //Get user Inputs
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //forward movement
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //left&right
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //rotation -- turning vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
